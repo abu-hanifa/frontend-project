10ad36276ef93e306fa78f5d5f7f6f8967a22490
Ismail <ismail@mail.ru>
2023-07-26 12:12:10 +0300

    Добавил Complete

--- src/Components/pages/Home/index.tsx
+++ src/Components/pages/Home/index.tsx
@@ -1,9 +1,12 @@
+import { Header } from 'antd/es/layout/layout'
 import React from 'react'
 import Category from '../../Category/Category'
+import '../../../App.css'
 
 const Home = () => {
   return (
     <div>
+      <Header/>
       <Category/>
     </div>
   )
