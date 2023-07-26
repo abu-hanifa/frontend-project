9f9c446bfd401c040cdef081fd33f41d7bfcadc1
Ismail <ismail@mail.ru>
2023-07-26 15:38:03 +0300

    some changes

--- src/Components/pages/Cart/index.tsx
+++ src/Components/pages/Cart/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Complete from './Complete'
+import ItemsReview from './ItemsReview'
+import styles from './Cart.module.css'
 
 const Cart = () => {
   return (
-    <div>
+    <div className={styles.mainContainer}>
+      <ItemsReview/>
       <Complete/>
     </div>
   )
