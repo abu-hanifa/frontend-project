280bca2c622b9abd50ddfe8f68d7c3151b0b242c
Ismail <ismail@mail.ru>
2023-07-28 16:44:32 +0300

    Create cart slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,9 +3,11 @@ import application from "../features/applicationSlice";
 import user from "../features/userSlice";
 import category from "../features/categorySlice";
 import cloth from "../features/clothSlice";
+import cart from '../features/cartSlice'
 
 export const store = configureStore({
   reducer: {
+    cart,
     category,
     application,
     user,
