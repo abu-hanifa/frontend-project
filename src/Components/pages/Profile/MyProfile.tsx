8a052377995d3f46fc577296c9640ba8a1a1a371
Sherlock0595 <magomed.umaev@mail.ru>
2023-07-30 17:43:34 +0300

    add lite validation

--- src/Components/pages/Profile/MyProfile.tsx
+++ src/Components/pages/Profile/MyProfile.tsx
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState, } from 'react';
 import styles from './Profile.module.css';
 import deleteIcon from '../../../assets/SvgIcons/delete.svg';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../../../app/store';
+import { getUser } from '../../../features/userSlice';
+import Eye from '../../../assets/SvgIcons/Eye.svg'
+
 
 const MyProfile = () => {
+
+  const user = useSelector((state: RootState) => state.user.user)
+  const dispatch = useDispatch()
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPassConfirm, setShowPassConfirm] = useState(false)
+  const [valuePasswordConfirm, setValuePasswordConfirm] = useState()
+  const [valuePassword, setvaluePassword] = useState()
+  const [passwordsMatch, setPasswordsMatch] = useState(true);
+
+  useEffect(() => {
+    dispatch(getUser())
+  }, [])
+
+  const handleFormChange = (e) => {
+    e.preventDefault();
+
+  }
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (valuePassword !== valuePasswordConfirm) {
+      setPasswordsMatch(false);
+      set
+    } else {
+      //здесь будет логика для отправки формы
+      setPasswordsMatch(true);
+    }
+  }
+
+  const handlePasswordChange = (e) => {
+    setvaluePassword(e.target.value)
+  }
+
+  const handleConfirmPasswordChange = (event) => {
+    setValuePasswordConfirm(event.target.value)
+  }
+  console.log(valuePasswordConfirm);
+
   return (
     <div>
       <div className={styles.titleProfile}>
@@ -11,35 +53,86 @@ const MyProfile = () => {
           <button> <img src={deleteIcon} alt="delete" />Delete account</button>
         </div>
       </div>
-      <form className={styles.profileForm}>
+      <form className={styles.profileForm} onSubmit={handleFormChange}>
         <div className={styles.formGroup}>
           <div><label htmlFor="first-name">First Name</label></div>
-          <input type="text" id="first-name" name="first-name" required />
+          <input type="text"
+            id="first-name"
+            name="first-name"
+            value={user.name}
+            required />
         </div>
 
         <div className={styles.formGroup}>
           <div><label htmlFor="last-name">Last Name</label></div>
-          <input type="text" id="last-name" name="last-name" required />
+          <input type="text"
+            id="last-name"
+            name="last-name"
+            value={user.subName}
+            required />
         </div>
 
         <div className={styles.formGroup}>
           <div><label htmlFor="email">Email</label></div>
-          <input type="email" id="email" name="email" required />
+          <input type="email"
+            id="email"
+            name="email"
+            required
+            value={user.email}
+          />
         </div>
 
         <div className={styles.formGroup}>
           <div><label htmlFor="phone">Phone</label></div>
-          <input type="tel" id="phone" name="phone" />
+          <input type="tel"
+            id="phone"
+            name="phone"
+            value={user.phone}
+          />
         </div>
 
         <div className={styles.formGroup}>
           <div><label htmlFor="new-password">New Password</label></div>
-          <input type="password" id="new-password" name="new-password" />
+          <div className={styles.wrappforInputAndBtn}>
+            <input type={showPassword ? 'text' : 'password'}
+              id="new-password"
+              name="new-password"
+              value={valuePassword}
+              onChange={(e) => handlePasswordChange(e)}
+            />
+            <button onClick={() => setShowPassword(!showPassword)}>
+              <img
+                src={Eye}
+                alt=""
+              />
+            </button>
+          </div>
+          {!passwordsMatch && (
+            <p style={{ color: 'red' }}>Пароли не совпадают!</p>
+          )}
         </div>
 
         <div className={styles.formGroup}>
-          <div><label htmlFor="confirm-password">Confirm Password</label></div>
-          <input type="password" id="confirm-password" name="confirm-password" />
+          <div><label
+            htmlFor="confirm-password">
+            Confirm Password
+          </label>
+          </div>
+
+          <div className={styles.wrappforInputAndBtn}>
+            <input type={showPassConfirm ? 'text' : 'password'}
+              id="confirm-password"
+              name="confirm-password"
+              value={valuePasswordConfirm}
+              onChange={(event) => handleConfirmPasswordChange(event)}
+            />
+            <button onClick={() => setShowPassConfirm(!showPassConfirm)}>
+              <img
+                src={Eye}
+                alt=""
+              />
+            </button>
+          </div>
         </div>
 
         <div className={styles.formGroup}>
@@ -54,14 +147,14 @@ const MyProfile = () => {
 
         <div className={styles.formGroup}>
           <div><label htmlFor="address">Address</label></div>
-          <textarea id="address" name="address" rows={3}></textarea>
+          <input id="address" name="address"></input>
         </div>
 
         <div className={styles.formGroup}>
           <div><label htmlFor="zip-code">ZIP Code</label></div>
           <input type="text" id="zip-code" name="zip-code" />
         </div>
-          <button className={styles.saveBtn}>Save changes</button>
+        <button onClick={handleFormSubmit} className={styles.saveBtn}>Save changes</button>
       </form>
     </div>
   );
