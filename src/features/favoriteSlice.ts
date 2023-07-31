564a1c25f0399bf685a3d6d3dcbd9370d9a4653a
lom-ali <lomali1999999@mail.ru>
2023-07-31 14:17:44 +0300

    0

--- src/features/favoriteSlice.ts
+++ src/features/favoriteSlice.ts
@@ -42,31 +42,6 @@ export const addFavorite = createAsyncThunk<
         Authorization: `Bearer ${thunkAPI.getState().application.token}`,
       },
     });
-
-    const json = await res.json();
-    if (json.error) {
-      return thunkAPI.rejectWithValue(json.error);
-    }
-    return json;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error);
-  }
-});
-
-export const deleteFavorite = createAsyncThunk<
-  ReturnType<typeof String>,
-  any,
-  { state: RootState }
->("favorite/fetchDeleteFavorite", async (id, thunkAPI) => {
-  try {
-    const res = await fetch(`http://localhost:4000/favorites/${id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${thunkAPI.getState().application.token}`,
-      },
-    });
-
     const json = await res.json();
     if (json.error) {
       return thunkAPI.rejectWithValue(json.error);
@@ -84,11 +59,11 @@ export const favoriteSlice = createSlice({
   extraReducers(builder) {
     builder
       .addCase(getUserFavorites.pending, (state) => {
-        state.clothes = {};
+        state.clothes = [];
         state.status = true;
       })
       .addCase(getUserFavorites.rejected, (state) => {
-        state.clothes = {};
+        state.clothes = [];
         state.status = false;
       })
       .addCase(getUserFavorites.fulfilled, (state, action: any) => {
@@ -102,18 +77,6 @@ export const favoriteSlice = createSlice({
         state.status = false;
       })
       .addCase(addFavorite.fulfilled, (state, action: any) => {
-        state.status = false;
-      })
-      .addCase(deleteFavorite.pending, (state) => {
-        state.status = true;
-      })
-      .addCase(deleteFavorite.rejected, (state) => {
-        state.status = false;
-      })
-      .addCase(deleteFavorite.fulfilled, (state, action) => {
-        state.clothes.cloth = state.clothes.cloth.filter(
-          (item) => item._id !== action.meta.arg
-        );
         state.status = false;
       });
   },
