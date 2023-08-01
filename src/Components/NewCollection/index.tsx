1df1cff2b63fd8bc1ac2f3572eda87bf3878ed4f
Rasvom <rasvomb@gmail.com>
2023-08-01 09:16:07 +0300

    code review

--- src/Components/NewCollection/index.tsx
+++ src/Components/NewCollection/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import styles from "./NewCollection.module.css";
+import { Link } from "react-router-dom";
+
 const NewCollection = () => {
   return (
     <div className={styles.NewCollection}>
       <div className={styles.content}>
         <div className={styles.collecText}>New Collection</div>
         <div className={styles.headerText}>Menswear 2023</div>
-        <button className={styles.shopBut}>Shop the menswear</button>
+        <Link to="/cloths/64bfbf396399ae3ace9dc58d">
+          <button className={styles.shopBut}>Shop the menswear</button>
+        </Link>
       </div>
       <div className={styles.backImage}></div>
     </div>
