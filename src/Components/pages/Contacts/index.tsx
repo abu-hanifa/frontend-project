2b3a6a96ceb6eeee9db81e74d851139ae883c61f
lom-ali <lomali1999999@mail.ru>
2023-08-01 15:04:34 +0300

    все

--- src/Components/pages/Contacts/index.tsx
+++ src/Components/pages/Contacts/index.tsx
@@ -1,24 +1,32 @@
-import React from 'react'
-import ContactsUs from './ContactsUs'
-import { Link, Route, Routes } from 'react-router-dom'
-import styles from './index.module.css'
-import FAQ from './FAQ'
+import React from "react";
+import ContactsUs from "./ContactsUs";
+import { Link, Route, Routes } from "react-router-dom";
+import styles from "./index.module.css";
+import FAQ from "./FAQ";
 
 const Contacts = () => {
   return (
-    <div className={styles.route} >
-      <Link className={styles.contact} to='/contact'><h3>ContactUs</h3></Link>
-      <Link className={styles.outlet} to='/contact/outlet'><h3>Outlet stores</h3></Link>
-      <Link className={styles.faq} to='/contact/faq'><h3>FAQ</h3></Link>
-      
-      <Routes>
-        <Route path="/" element={<ContactsUs/>}/>
-        
-        <Route path='/faq' element={<FAQ/>}/>
-      </Routes>
-      
-    </div>
-  )
-}
+    <div className={styles.route}>
+      <div>
+        <Link className={styles.contact} to="/contact">
+          <h3>ContactUs</h3>
+        </Link>
+        <Link className={styles.outlet} to="/contact/outlet">
+          <h3>Outlet stores</h3>
+        </Link>
+        <Link className={styles.faq} to="/contact/faq">
+          <h3>FAQ</h3>
+        </Link>
+      </div>
+      <div>
+        <Routes>
+          <Route path="/" element={<ContactsUs />} />
 
-export default Contacts
+          <Route path="/faq" element={<FAQ />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default Contacts;
